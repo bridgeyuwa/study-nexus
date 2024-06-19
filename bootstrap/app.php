85a4895ce5a684144860af847719b8c455ca2e01
bridgeyuwa <torkumajonathanyuwa@gmail.com>
2024-06-19 19:36:15 +0100

    Exempted Livewire/update from Csrf validation
    Exempted Livewire/update from Csrf validation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+		$middleware->validateCsrfTokens(except: [
+		'livewire/update',
+		]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
