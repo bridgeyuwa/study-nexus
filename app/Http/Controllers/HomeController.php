2d7b94d96627573479e1f8bf93e892bf21621b8f
bridgeyuwa <torkumajonathanyuwa@gmail.com>
2024-07-02 15:55:34 +0100

    removed unecessary Models

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,28 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\State;
-use App\Models\College;
 use App\Models\Institution;
 use App\Models\Program;
-use App\Models\Region;
+use App\Models\Category;
+use App\Models\Level;
 
 class HomeController extends Controller {
 
     public function index() {
 
-        $states = State::all();
-        $colleges = College::all();
         $institutions = Institution::all();
         $programs = Program::all();
-        $regions = Region::all();
+		$categories = Category::all();
+		$levels = Level::all(); 
         
-        
-        $rankedInstitutions = Institution::whereNotNull('rank')->orderBy('rank')->limit(6)->get();
-        $randomPrograms = Program::inRandomOrder()->limit(6)->get();
-        $randomStates = State::inRandomOrder()->limit(6)->get();
-        
-       return view('home', compact('states','regions', 'colleges','institutions','programs','rankedInstitutions','randomStates','randomPrograms'));
+       return view('home', compact('institutions','programs','categories','levels'));
     }
 
 }
