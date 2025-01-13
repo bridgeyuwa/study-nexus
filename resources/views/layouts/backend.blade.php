0ec47c3c4cb50f52c8456fbc59c92a1f64c99c5d
bridgeyuwa <bridgeyuwa@gmail.com>
2025-01-13 18:53:15 +0100

    Added Google Analytics

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -1,8 +1,19 @@
-
 <!doctype html>
 <html lang="{{ config('app.locale') }}">
 
 <head>
+    @if (app()->environment('production'))
+      <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JRK9XM0YXN"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-JRK9XM0YXN');
+        </script>    
+    @endif
+    
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
