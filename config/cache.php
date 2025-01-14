851cb2099786d88363f4a995c9201b5efa7c6b92
bridgeyuwa <bridgeyuwa@gmail.com>
2025-01-14 16:09:15 +0100

    Attempt to re-enable database cache

--- config/cache.php
+++ config/cache.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('CACHE_STORE', null),
+    'default' => env('CACHE_STORE', 'database'),
 
     /*
     |--------------------------------------------------------------------------
@@ -87,10 +87,6 @@ return [
 
         'octane' => [
             'driver' => 'octane',
-        ],
-		
-		'none' => [
-            'driver' => null,
         ],
 
     ],
